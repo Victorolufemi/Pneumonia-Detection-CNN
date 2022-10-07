b173d929060603b110131575c9b6b5e02b53fc21
Victorolufemi <Olufe17@gmail.com>
2022-10-07 04:44:43 +0100

    writing the web application backend update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from crypt import methods
 from email.mime import image
+from tkinter import image_names
 from tkinter.ttk import Style
+from unittest import result
 from keras.models import load_model
 from keras.models import model_from_json
 from flask import Flask,url_for,render_template,redirect,session,Response
@@ -27,4 +30,53 @@ json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
 loaded_model.load_weights("model/model.h5")
+
+#Create the predict function
+img_size = 150
+def predict(model,sample):
+    img = cv2.imread(sample)
+    img = cv2.resize(img,(img_size, img_size))
+    predicts = (model.predict(img)> 0.5).astype("int32")
+    predictions = predicts.reshape(1,-1)[0]
+    return predictions
+
+
+def classify(value):
+    if value==0:
+        return 'The Patient has no Pneumonia Disease'
+    elif value==1:
+        return 'The Patient has a pneumonia Disease, Kindly go the nearest Hospital for checkup'
+
+
+x=0
+
+@app.route('/', methods=['GET','POST'])
+
+def index():
+    form = Detect_form()
+
+    if form.validate_on_submit():
+
+        assets_dir = './static'
+        imgs =form.image.data
+        img_name = secure_filename(imgs.filename)
+
+        img.save(os.path.join(assets_dir,img_name))
+        global x
+        x = os.path.join(assets_dir, img_name)
+
+        return redirect(url_for('prediction'))
+    return render_template('home.html', form=form)
+
+@app.route('/result')
+def prediction():
+
+    pred_val = predict(loaded_model, x)
+    result = classify(pred_val)
+    os.remove(x)
+    return render_template('prediction.html', result=result)
+
+
+if __name__== '__main__':
+    app.run()
 

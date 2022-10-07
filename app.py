b158b2e95d39cd26a1bcba38964df1de7ff1f0c0
Victorolufemi <Olufe17@gmail.com>
2022-10-07 04:06:57 +0100

    writing the web application backend update

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from keras.models import load_model
+from keras.models import model_from_json
 from flask import Flask,url_for,render_template,redirect,session,Response
-from flask_wtf import 
+from flask_wtf import FlaskForm
+from wtforms import FileField,SubmitField
+from flask_wtf.file import file_allowed,file_required
+from werkzeug.utils import secure_filename
+import cv2
+import numpy
+import os
